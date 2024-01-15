10503867e41f446117336220996d09e28871b426
Kevin Cerro DVRV <kevincerro.dvrv@gmail.com>
2024-01-15 20:25:58 +0100

    Apply last class changes

--- Assets/Scripts/OffsetGrabInteractable.cs
+++ Assets/Scripts/OffsetGrabInteractable.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
-public class OffsetGrabInteractable : XRGrabInteractable
-{
+public class OffsetGrabInteractable : XRGrabInteractable {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         GameObject attach = new GameObject("Attach");
         attach.transform.parent = transform;
         attachTransform = transform;
     }
 
-    protected override void OnSelectEntering(XRBaseInteractor interactor)
-    {
+    protected override void OnSelectEntering(XRBaseInteractor interactor) {
+         base.OnSelectEntering(interactor);
         attachTransform.position = interactor.transform.position;
         attachTransform.rotation = interactor.transform.rotation;
 
-        base.OnSelectEntering(interactor);
+       
     }
 }
